213e189abf23b8fbc1b04c1dba6c9c30f6e41aaa
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2021-11-02 16:12:43 +0100

    Sample zur N-Tier-Verwaltungsfrage

---
+++ domain-layer/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module domain.layer {
+    exports de.datev.schulung.java.domain;
+}
---
+++ presentation-layer/src/main/java/de/datev/schulung/java/presentation/PersonGui.java
@@ -0,0 +1,17 @@
+package de.datev.schulung.java.presentation;
+
+import de.datev.schulung.java.domain.Person;
+
+public class PersonGui {
+
+    Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+}
